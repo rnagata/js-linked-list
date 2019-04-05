b53427b977bf858506ecffc9bd9f16faab0e71bd
Ronald J Nagata Jr <junzo@Ronalds-MBP.home>
2019-04-05 08:50:02 -1000

    Insert and Remove incomplete

--- linkedList.js
+++ linkedList.js
@@ -35,20 +35,48 @@ function linkedListGenerator(){
     tail = newNode;
   }
 
-  function remove(){
-
+  function remove(number){
+    
+    console.log(number);
+    console.log(head);
+    let node = head;
+    let count = 0;
+    let preceedingNode;
+    let followingNode;
+    while (node){
+      console.log(count, node.value);
+      if (count === number - 1){
+        preceedingNode = node;
+      }
+      if (count === number + 1){
+        followingNode = node;
+      }
+      node = node.next;
+      count++;
+    }
+    if (followingNode){
+      preceedingNode.next = followingNode;
+      tail = followingNode;
+      return; 
+    }
+    preceedingNode.next = null;
+    tail = preceedingNode;
+    if (number === 0){
+      head = head.next;
+      
+    }
+    console.log('end: ', head);
   }
 
   function get(number){
-    currentNode = head;
+    let node = head;
     for (let i = 0; i < number; i++){
-      console.log(currentNode.value);
-      currentNode = currentNode.next;
-      if (currentNode.next === null){
+      if (!node.next){
         return false;
       }
+      node = node.next;
     }
-    return currentNode;
+    return node;
   }
 
   function insert(){}
